63f5510624832e8d4fc968672d5289c8a0c8d4f6
李德永 <lideyong@newcapec.net>
2021-12-13 14:02:41 +0800

    main删除demo

---
+++ tslearn/interface.js
@@ -0,0 +1,5 @@
+"use strict";
+function getPort(nane) {
+    console.log(nane);
+}
+getPort({ masi: 'name_port' });
---
+++ tslearn/interface.ts
@@ -0,0 +1,13 @@
+// 传入约束
+// 批量约束
+// 
+ interface Port{
+   masi?: string
+ }
+
+ function getPort(nane: Port){
+  console.log(nane);
+ }
+
+ getPort({masi:'name_port'})
+ 
